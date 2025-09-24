427b7e32aacf940ec2313d58832ca440c6bac825
Ammar Al-Baghajati <AmmarAlBaghajati@gmail.com>
2025-09-24 14:48:56 +0300

    implimented the country's BLL&DAL

--- DVLD/DVLD.BLL/Entities/Country.cs
+++ DVLD/DVLD.BLL/Entities/Country.cs
@@ -8,5 +8,31 @@ namespace DVLD.BLL.Entities
 {
     internal class Country
     {
+        private enum Mode
+        {
+            Update,
+            AddNew
+        };
+
+        private Mode mode;
+
+        public int ID { get; private set; }
+        public string CountryName { get; set; }
+
+        private Country(int iD,string countryName)
+        {
+            mode = Mode.Update;
+            ID = iD;
+            CountryName = countryName;
+        }
+
+        public Country()
+        {
+            mode = Mode.AddNew;
+            ID = 0;
+            CountryName = string.Empty;
+        }
+
+
     }
 }
--- DVLD/DVLD.DAL/Data/CountryData.cs
+++ DVLD/DVLD.DAL/Data/CountryData.cs
@@ -4,16 +4,196 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using Microsoft.Data.SqlClient;
+using DVLD.DAL.DTO;
 
 namespace DVLD.DAL.Data
 {
-    internal class CountryData
+    public class CountryData
     {
         private static object HandleNull(string? value)
         {
             return string.IsNullOrEmpty(value) ? DBNull.Value : value;
         }
 
-        public static DataTable
+        public static DataTable GetByID(int iD)
+        {
+            using var conn = new SqlConnection();
+
+            string query = @"SELECT * FROM Countries
+                            WHERE ID = @ID";
+
+            using var cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@ID", iD);
+
+            DataTable dt = new DataTable();
+            try
+            {
+                conn.Open();
+
+                using var reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                    dt.Load(reader);
+
+                return dt;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Occurred from the database!", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Occurred in DAL!", ex);
+            }
+        }
+
+        public static int Add(CountryDTO country)
+        {
+            using var conn = new SqlConnection(Connection.ConnectionString);
+
+            string query = @"INSERT INTO Countries (CountryName)
+                            VALUES 
+							(@CountryName)
+
+							SELECT SCOPE_IDENTITY();";
+
+            using var cmd = new SqlCommand(query,conn);
+            cmd.Parameters.AddWithValue("@CountryName", country.CountryName);
+
+            try
+            {
+                conn.Open();
+
+                object result = cmd.ExecuteScalar();
+
+                if(result != null && int.TryParse(result.ToString(),out int iD))
+                    return iD;
+
+                return 0;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Occurred from the database!", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Occurred in DAL!", ex);
+            }
+        }
+        public static int Update(CountryDTO country)
+        {
+            using var conn = new SqlConnection(Connection.ConnectionString);
+
+            string query = @"UPDATE Countries 
+                            SET
+                            CountryName = @CountryName
+                            WHERE ID = @ID;";
+
+            using var cmd = new SqlCommand(query,conn);
+            cmd.Parameters.AddWithValue("@CountryName", country.CountryName);
+            cmd.Parameters.AddWithValue("@ID", country.ID);
+
+            try
+            {
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                return rows;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Occurred from the database!", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Occurred in DAL!", ex);
+            }
+        }
+        public static int Delete(int iD)
+        {
+            using var conn = new SqlConnection(Connection.ConnectionString);
+
+            string query = @"DELETE FROM Countries
+                            WHERE ID = @ID;";
+
+            using var cmd = new SqlCommand(query,conn);
+            cmd.Parameters.AddWithValue("@ID",iD);
+
+            try
+            {
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                return rows;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Occurred from the database!", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Occurred in DAL!", ex);
+            }
+
+        }
+        public static DataTable GetAll()
+        {
+            using var conn = new SqlConnection(Connection.ConnectionString);
+
+            string query = @"SELECT * FROM Countries;";
+
+            using var cmd = new SqlCommand(query, conn);
+
+            try
+            {
+                conn.Open();
+
+                DataTable dt = new DataTable();
+
+                using var reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                    dt.Load(reader);
+
+                return dt;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Occurred from the database!", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Occurred in DAL!", ex);
+            }
+        }
+        public static bool IsExist(int iD)
+        {
+            using var conn = new SqlConnection(Connection.ConnectionString);
+
+            string query = @"SELECT 1 FROM Countries
+                            WHERE ID = @ID;";
+
+            using var cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@ID", iD);
+            try
+            {
+                conn.Open();
+
+                object result = cmd.ExecuteScalar();
+
+                return result != null;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error Occurred from the database!", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Occurred in DAL!", ex);
+            }
+        }
     }
 }
